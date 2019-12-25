d11b9aa3bff3a469d616f2b663f5161e24a81648
fei5631 <526092972@qq.com>
2019-12-25 17:31:50 +0800

    创建目录

---
+++ DYZB/class/main/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  DYZB
+//
+//  Created by ZhongDe on 2019/12/25.
+//  Copyright © 2019 ZhongDe. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(1)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
